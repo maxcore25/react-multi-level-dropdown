94005ff7dbef13cefc4a024786c5f1a24547e915
maxcore25 <mkorobov2002@mail.ru>
2022-04-15 15:27:55 +0300

    SVG Created And Dropdown Components Copied

--- src/App.js
+++ src/App.js
@@ -1,18 +1,125 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { ReactComponent as Icon } from './icons/star.svg';
+import { CSSTransition } from 'react-transition-group';
+
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+    <Navbar>
+      <NavItem icon={<Icon />} />
+      <NavItem icon={<Icon />} />
+      <NavItem icon={<Icon />} />
+
+      <NavItem icon={<Icon />}>
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
+    </Navbar>
+  );
+}
+
+function Navbar(props) {
+  return (
+    <nav className='navbar'>
+      <ul className='navbar-nav'>{props.children}</ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className='nav-item'>
+      <a href='#' className='icon-button' onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+
+      {open && props.children}
+    </li>
+  );
+}
+
+function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+  }, []);
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  function DropdownItem(props) {
+    return (
+      <a
+        href='#'
+        className='menu-item'
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+        <span className='icon-button'>{props.leftIcon}</span>
+        {props.children}
+        <span className='icon-right'>{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className='dropdown' style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === 'main'}
+        timeout={500}
+        classNames='menu-primary'
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className='menu'>
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<Icon />}
+            rightIcon={<Icon />}
+            goToMenu='settings'>
+            Settings
+          </DropdownItem>
+          <DropdownItem leftIcon='🦧' rightIcon={<Icon />} goToMenu='animals'>
+            Animals
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'settings'}
+        timeout={500}
+        classNames='menu-secondary'
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className='menu'>
+          <DropdownItem goToMenu='main' leftIcon={<Icon />}>
+            <h2>My Tutorial</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<Icon />}>HTML</DropdownItem>
+          <DropdownItem leftIcon={<Icon />}>CSS</DropdownItem>
+          <DropdownItem leftIcon={<Icon />}>JavaScript</DropdownItem>
+          <DropdownItem leftIcon={<Icon />}>Awesome!</DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'animals'}
+        timeout={500}
+        classNames='menu-secondary'
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className='menu'>
+          <DropdownItem goToMenu='main' leftIcon={<Icon />}>
+            <h2>Animals</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon='🦘'>Kangaroo</DropdownItem>
+          <DropdownItem leftIcon='🐸'>Frog</DropdownItem>
+          <DropdownItem leftIcon='🦋'>Horse?</DropdownItem>
+          <DropdownItem leftIcon='🦔'>Hedgehog</DropdownItem>
+        </div>
+      </CSSTransition>
     </div>
   );
 }
